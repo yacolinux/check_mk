2c25b48d2a3174b3ebec92fac9fdf20543c666e0
cfmjohn <mcculloch.john@gmail.com>
2013-12-29 22:11:56 +0000

    Tweak formatting issues from editor.

--- notify_by_pushover.sh
+++ notify_by_pushover.sh
@@ -2,14 +2,14 @@
 # Notify by Pushover
 
 #	Notify by Pushover for Check_MK
-#   adapted by John McCulloch
+#	adapted by John McCulloch
 #
 #	originally by Jedda Wignall
 #	http://jedda.me
 
 #	v1.3.0 - 29 Dec 2013
 #	Initial Check_MK adaptation.
-#   Removing explicit sound settings to favour user preferences in client app.
+#	Removing explicit sound settings to favour user preferences in client app.
 
 #	v1.2 - 18 Dec 2012
 #	Added parsing of title for specific warning, critical, and OK sounds.
@@ -27,7 +27,7 @@
 #	as argument 1 in Check_MK's flexible notifications config. You can register an app once logged into Pushover at the following link:
 #	https://pushover.net/apps/build
 #
-#   This script is set up to look for the Pushover user key in the Pager Address/Address2 field.
+#	This script is set up to look for the Pushover user key in the Pager Address/Address2 field.
 
 # 	You also need to provide your Pushover application key as the first argument in the Check_MK notification config.
 
@@ -57,13 +57,12 @@ esac
 # Check if the service state is set. If not then don't show service details in msg.
 if [[ -z "$NOTIFY_SERVICESTATE" ]]
 then
-    # Service details not set.
-    title="$hostAlias $hostState"
-    message=$hostAlias' is '$hostState$'\n'$hostOutput
-    priority=1
+	# Service details not set.
+	title="$hostAlias $hostState"
+	message=$hostAlias' is '$hostState$'\n'$hostOutput
 else
 	title="$hostAlias $hostState - $serviceDesc $serviceState"
-    message=$'Service:\n'$serviceDesc$'\n'$serviceState$'\n'$serviceOutput$'\nHost:\n'$hostAlias' is '$hostState$'\n'$hostOutput
+	message=$'Service:\n'$serviceDesc$'\n'$serviceState$'\n'$serviceOutput$'\nHost:\n'$hostAlias' is '$hostState$'\n'$hostOutput
 fi
 
 curl -F "token=$appToken" \
